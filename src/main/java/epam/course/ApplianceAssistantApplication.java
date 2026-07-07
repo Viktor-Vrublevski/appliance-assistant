565bddb12ce6adc858128e41c253240419dd0ced
viktaru <zestaw2017@gmail.com>
2026-07-07 08:00:31 +0200

    Initial commit

---
+++ src/main/java/epam/course/ApplianceAssistantApplication.java
@@ -0,0 +1,13 @@
+package epam.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplianceAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplianceAssistantApplication.class, args);
+	}
+
+}
